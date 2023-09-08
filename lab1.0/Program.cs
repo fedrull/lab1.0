369fef91ff7469a6225cfe9defda6a84d7f3a53e
felix <felix@DESKTOP-PCU8J78>
2023-09-08 12:30:24 +0200

    Add project files.

---
+++ lab1.0/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+
+namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string");
+            string input = Console.ReadLine();
+            List<string> strings = new List<string>();
+
+            for (int startvalue = 0; startvalue < input.Length - 1; startvalue++)
+            {
+                for (int j = startvalue + 1; j < input.Length; j++)
+                {
+                    if (char.IsLetter(input[j]))
+                    {
+                        break;
+                    }
+
+                    if (input[startvalue] == input[j])
+                    {
+                        strings.Add(input[startvalue..(j + 1)]);
+                        break;
+                    }
+                }
+            }
+
+            strings.ForEach(s =>
+            {
+                Console.Write(input[0..input.IndexOf(s, 0)]);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(s);
+                Console.ResetColor();
+                Console.WriteLine(input[(input.IndexOf(s, 0) + s.Length)..^0]);
+                
+                
+                
+            });
+
+            long sum = strings.Select(s => long.Parse(s)).Sum();
+            Console.WriteLine($"The total value is { sum }");
+
+        }
+    }
+}
